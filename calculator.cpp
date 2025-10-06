d7c6ae1ec0b8362c521f9c11d509cc5faa7535d6
Sonik1238 <kulichenko.sofia@lll.kpi.ua>
2025-10-06 16:52:49 +0300

    feat: add comments and division operation

--- calculator.cpp
+++ calculator.cpp
@@ -1,16 +1,25 @@
 #include "calculator.h"
 
+//Addition operation
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+//Subtraction operation
 int Calculator::Sub (double a, double b)
 {
     return Add (a, -b);
 }
 
+//Multiplication operation
 int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
+}
+
+//Division operation
+int Calculator::Mul (double a, double b)
+{
+    return a / b + 0.5;
 }
